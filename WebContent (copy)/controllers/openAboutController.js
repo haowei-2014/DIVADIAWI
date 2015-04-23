139d34ae576ae6b5fa5b09b8b9056d3d4106da42
HaoWei <hao.wei@unifr.ch>
2015-04-23 06:08:50 -0700

    delete WebContent(copy)

--- WebContent (copy)/controllers/openAboutController.js
+++
@@ -1,9 +0,0 @@
-myApp.controller('openAboutController', ['$scope', 'close',
-        function ($scope, close) {
-            $scope.close = function () {
-                $('.modal-backdrop').remove();
-                close({
-                    about: "This is about.html",
-                }, 500); 
-            };
-    }]);
